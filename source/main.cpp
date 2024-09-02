2ecd38df88b0f0a7d9a74c6db40d22c06da308f4
Jaime Rios <3794310+AhiyaHiya@users.noreply.github.com>
2024-09-02 13:51:24 -0700

    Add GetStreetAddress function

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,26 @@
+
 #include <iostream>
+#include <string>
+
+auto GetStreetAddress()
+{
+    std::cout << "You will need to input a street address\n";
+    std::string line;
+    std::getline(std::cin, line);
+    std::cout << "You entered: \n\t" << line << "\n";
+    return line;
+}
 
 int main(int, char**)
 {
     std::cout << "Hello, from GetGeoCoords!\n";
+
+    auto streetAddress = GetStreetAddress();
+    if (streetAddress.empty())
+    {
+        std::cout << "No street address to work with. Exiting app\n";
+        return 1;
+    }
+
     return 0;
 }
